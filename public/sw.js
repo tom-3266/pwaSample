6d997388209f4265899c8c205bab82b2160d6289
Tom <tomsibimattathil@gmail.com>
2024-09-23 12:59:08 +0530

    added service worker

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", function (event) {
+  console.log("[Service worker] installing service worker", event);
+});
+
+self.addEventListener("activate", function (event) {
+  console.log("[Service worker] activating service worker", event);
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  console.log("[Service worker] fetching something...", event);
+  event.respondWith(fetch(event.request));
+});
